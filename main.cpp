3fffeb5f8a7afa5b3bb8cbd226f0eee611c359c7
fallenazura003 <78076005+fallenazura003@users.noreply.github.com>
2021-05-05 10:59:00 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+﻿#include "snake.hpp"
+#include <iostream>
+#include <stdexcept>
+
+int main(int argc, char **argv)
+{
+
+  try
+  {
+    Snake s;
+    return s.run();
+  } catch (std::exception &e) //bắt exception
+  {
+    std::cerr << e.what() << std::endl;
+    return 1;
+  }
+}
---
+++ snake.hpp
@@ -0,0 +1,36 @@
+﻿#include <SDL.h>
+#include<SDL_mixer.h>
+#include <deque> //cho phép nhận nhiều thao tác.
+
+class Snake 
+{
+public:
+  Snake();
+  ~Snake();
+  int run();
+  bool tick();
+  void draw();
+  static const auto HeadOpenMouth = 0;
+  static const auto Tail = 1;
+  static const auto Turn = 2;
+  static const auto Straight = 3;
+  static const auto Head = 4;
+  static const auto Fruit = 5;
+  const static auto Width = 1280;
+  const static auto Height = 600;
+private:
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+  SDL_Texture *sprites;
+  Mix_Chunk* chunk = NULL;
+  Mix_Music* music = NULL;
+  std::deque<std::pair<int, int> > segmentsList;
+  unsigned ticks = 0;
+  int dx = 1;
+  int dy = 0;
+  int fruitX;
+  int fruitY;
+  void generateFruit();
+  void loadmusic();
+  void loadchunk();
+};
